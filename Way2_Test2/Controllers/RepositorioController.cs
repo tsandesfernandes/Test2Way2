58de2cc9c07206c239dc3090479a15ecf9603728
Tiago Fernandes <tsandesfernandes@gmail.com>
2015-02-07 18:57:44 -0200

    initial commit

---
+++ Way2_Test2/Controllers/RepositorioController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Octokit;
+using System.Threading.Tasks;
+
+namespace Way2_Test2.Controllers
+{
+    public class RepositorioController : Controller
+    {
+        //personal access token = e9bff4045442213ae0d91b0d9e8c200147342515 
+        GitHubClient github = new GitHubClient(new ProductHeaderValue("test"), new Uri("https://github.com/")) {
+            Credentials = new Credentials("e9bff4045442213ae0d91b0d9e8c200147342515") 
+        };
+
+        //
+        // GET: /Repositorio/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> myRespositores()
+        {
+            //var repositories = await github.Repository.GetAllForCurrent() as IEnumerable<Object>;
+            var repositories = await github.Repository.GetAllForCurrent() as IEnumerable<Repository>;
+            return View(repositories.ToList());
+                
+        }
+
+        public async Task<ActionResult> searchRepository(string name)
+        {
+            var repositories = await github.Search.SearchRepo(new SearchRepositoriesRequest(name));
+            return View(repositories.Items.ToList());
+        }
+        public async Task<ActionResult> favoriteRepositories()
+        {
+
+            var repositories = await new StarredClient(new ApiConnection(github.Connection)).GetAllForCurrent();
+            return View(repositories.ToList());
+        }
+
+
+
+    }
+}
